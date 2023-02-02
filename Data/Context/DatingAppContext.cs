9b57a52ed9a7594b9a6e3358bf4a6df306070e5c
USER <USER@DESKTOP-20DRNS1>
2023-02-03 03:21:44 +0330

    Add project files.

---
+++ Data/Context/DatingAppContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class DatingAppContext : DbContext
+    {
+        public DatingAppContext(DbContextOptions<DatingAppContext> options)
+            : base(options)
+        {
+
+        }
+
+
+
+        //dbsets
+        public DbSet<User> Users { get; set; }
+    }
+}
