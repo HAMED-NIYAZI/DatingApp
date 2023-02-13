ba61adb55d8b22192dea38d06f8ab1bf7305bdce
USER <USER@DESKTOP-20DRNS1>
2023-02-13 22:16:52 +0330

    Implement CreateToken

--- DatingApp.Api/Services/Implementation/TokenService.cs
+++ DatingApp.Api/Services/Implementation/TokenService.cs
@@ -1,14 +1,21 @@
 ﻿using DatingApp.Api.Services.Interface;
 using Domain.Entities.User;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace DatingApp.Api.Services.Implementation
 {
     public class TokenService : ITokenService
     {
-        #region ctor
-        public TokenService()
-        {
 
+        #region ctor
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
         }
 
         #endregion
@@ -17,7 +24,23 @@ namespace DatingApp.Api.Services.Implementation
 
         public string CreateToken(User user)
         {
-            throw new NotImplementedException();
+            var claim = new List<Claim>
+            {
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.NameId,user.UserName)
+            };
+            var creds=new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            { 
+            Subject = new ClaimsIdentity(claim),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token=tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
         }
 
         #endregion
